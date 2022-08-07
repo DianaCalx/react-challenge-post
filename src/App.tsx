1687baf7268286c4573cc64772aa8e39335eb574
Diana Calderon <dianacalderon@ravn.co>
2022-08-07 10:09:27 -0600

    Add route management

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import Login from './components/Login';
+import { Layout } from './components/Layout';
 
 const App = () => {
   return (
     <Router>
-      <Routes>
+      <Routes>  
+        <Route
+          path="/posts"  
+          element={<Layout />}   
+        />
+        <Route path="posts/:id" element={<Layout />}/>
         <Route
           path="/"
           element={<Login/>}
-        />
-        <Route 
-          path="/post"  
-          element={<div>Hi</div>}   
         />
       </Routes>
     </Router>
