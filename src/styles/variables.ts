d63ae6924465e6acfff80e0a77f4e53c90d5cd26
Diana Calderon <dianacalderon@ravn.co>
2022-08-07 10:19:02 -0600

    Add variables for colors

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -1,6 +1,11 @@
 const variables = {
   primaryColor: '#B2A4FF',
-  secondaryColor: '#FFB4B4'
+  secondaryColor: '#FFDEB4',
+  tertiaryColor: '#FF7396',
+  fourthColor: '#FF4977',
+  neutralColor1: '#c7c7c7',
+  shadowColor: '#382B80',
+  shadowColor2: '#762599'
 }
 
 export default variables;
